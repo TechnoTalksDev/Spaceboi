37d4566b5b427215c9e46bcaa8c739b134f8606f
TechnoTalksDev <technotalksyt101@gmail.com>
2022-03-27 18:28:27 -0700

    Implemented damage for enemies

--- main.py
+++ main.py
@@ -26,8 +26,11 @@ pygame.display.set_caption("SpaceBoi")
 bgcolor = (45, 49, 52)
 player_image = pygame.image.load("assets/ship.png").convert_alpha()
 scout_image = pygame.image.load("assets/scout.png").convert_alpha()
+scout_image_damaged = pygame.image.load("assets/scout_damaged.png").convert_alpha()
 button_down_main = False
 button_down_secondary = False
+font = pygame.font.Font("freesansbold.ttf", 32)
+missile_delay=0
 @dataclass
 class Enemies:
     scouts=[]
@@ -75,12 +78,13 @@ class PlayerBullet:
         self.x_vel = math.cos(self.angle)*self.speed
         self.y_vel = math.sin(self.angle)*self.speed
         self.color = color
+        self.rect = None
     def main(self, window):
         self.x -= int(self.x_vel)
         self.y -= int(self.y_vel)
         #0, 129, 250
-        pygame.draw.circle(window, self.color, (self.x, self.y), 5)
-
+        self.rect=pygame.draw.circle(window, self.color, (self.x, self.y), 5)
+#Scout enemy
 class Scout:
     def __init__(self):
         self.x = random.randint(0, 1280)
@@ -90,21 +94,28 @@ class Scout:
         self.x_vel = math.cos(self.angle)*self.speed
         self.y_vel = math.sin(self.angle)*self.speed
         self.color = (255, 166, 0)
-        self.shields = 50
-        self.health = 25
+        self.shields = 30
+        self.health = 20
+        self.rect = None
+        self.damage = False
+        self.type = "scout"
     def main(self, window):
         self.angle = math.atan2(self.y-player_location.y, self.x-player_location.x)
         self.x_vel = math.cos(self.angle)*self.speed
         self.y_vel = math.sin(self.angle)*self.speed
         self.x -= int(self.x_vel)
         self.y -= int(self.y_vel)
-
         rel_x, rel_y = player_location.x - self.x, player_location.y - self.y
         angle_to_pointer =(180 / math.pi) * -math.atan2(rel_y, rel_x)
         angle_to_pointer += 90
-        img_copy = pygame.transform.rotate(scout_image, angle_to_pointer)
+        if not self.damage:
+            img_copy = pygame.transform.rotate(scout_image, angle_to_pointer)
+        else:
+            img_copy = pygame.transform.rotate(scout_image_damaged, angle_to_pointer)
+            self.damage = False
         window.blit(img_copy, (self.x-int(img_copy.get_width()/2), self.y-int(img_copy.get_height()/2)))
-
+        self.rect = pygame.Rect((self.x-20, self.y-20), (scout_image.get_width(), scout_image.get_height()))
+#event handler
 def event_handler(event):
     global button_down_main, button_down_secondary
     if event.type == pygame.QUIT:
@@ -148,16 +159,34 @@ class gun:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             Bullets.bullets.append(PlayerBullet(player_location.x, player_location.y, mouse_x, mouse_y, 15, (250, 208, 0)))
             last_shot.machine_gun = time.time()
+    def machine_gun_bullets(enemy):
+        for bullet in Bullets.bullets:
+            if bullet.rect.colliderect(enemy.rect):
+                enemy.damage = True
+                Bullets.bullets.remove(bullet)
+                if enemy.type == "scout":
+                    if enemy.shields <= 0:
+                        enemy.health -= 5
+                    else:
+                        enemy.shields -= 5
+                    if scout.health <= 0:
+                        Enemies.scouts.remove(enemy)
     def missile():
+        global missile_delay
         current_time=time.time()
-        delay = current_time - last_shot.missile
+        missile_delay = current_time - last_shot.missile
         #print(delay)
-        if delay > 1:
+        if missile_delay > 1:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             Bullets.missiles.append(PlayerBullet(player_location.x, player_location.y, mouse_x, mouse_y, 15, (0, 129, 250)))
-            last_shot.missile = time.time()     
-    
-Enemies.scouts.append(Scout())
+            last_shot.missile = time.time()
+    def missile_logic(enemy):
+        for missile in Bullets.missiles:
+            if missile.rect.colliderect(enemy.rect):
+                enemy.damage = True
+                Bullets.missiles.remove(missile)
+                if enemy.type == "scout":
+                    Enemies.scouts.remove(enemy)     
 
 while True:
     window.fill(bgcolor) #clear screen with backround color
@@ -190,6 +219,10 @@ while True:
 
     for scout in Enemies.scouts:
         scout.main(window)
+        pygame.draw.rect(window, (0,0,0), scout.rect, 1)
+        gun.machine_gun_bullets(scout)
+        gun.missile_logic(scout)
+
     #Bonk
     pygame.display.update() #update *ENTIRE* display
     clock.tick(60) #60 FPS gamers 
